8d03766fc4182495da88117efa1d70b29ec70bf8
Thibaud <thibaud@debian>
2017-07-31 11:38:11 +0200

    passage du % en em sur le css

--- cvthibaud.php
+++ cvthibaud.php
@@ -8,103 +8,93 @@
   </head>
 
 <body>
-    
-    
-    
-<!-----------------------------------------------------------------><div class = "divleft"><!----------------------------------------------------------------->                         
-        
-        
-        
+
+
+
+<!-----------------------------------------------------------------><div class = "divleft"><!----------------------------------------------------------------->
+
+
+
                     <h1 class="wrapper" id="top">Bousquainaud Thibaud<span class="policedeveloppeur">développeur web</span></h1>
 
-    
+
         <p class = "pImage">
             <img src="images/thibaudfacebook.png"
               class="image" alt="image">
         </p>
-                                    
-                                    
-<div id="conteneur">
+
+
+<div id="conteneurCitation">
 
     <div class="element1">"Codez toujours comme si la personne qui allait</div>
 
     <div class="element2">maintenir votre code était un violent</div>
 
     <div class="element3">psychopathe qui sait où vous habitez."</div>
-                    
+
     <div class="element4">― <strong>John Woods</strong> </div>
 
 </div>
-                                    
-                                    
 
-<<<<<<< HEAD
-  </header>
-
-  <div class="progress-bar blue"></div>
-
-   <article class="wrapper">
-=======
 <aside class = "aside-left">
-    
->>>>>>> c8c1fdaae346636ae6bd707c6a76e5093da3ed6b
+
     <div class= "coordonnes">
-        
-        <article>
-            
-         <main><p>44 chemin des ducs 26190 Saint Jean en Royans</p></main> 
-            <aside class = "icone"><p class = "pImage"><img class = "icone1" 
-                                        src="images/adresse.jpg" 
+
+        <article class = "coordonnes1">
+
+         <main><p>44 chemin des ducs</p> <p>26190 Saint Jean en Royans</p></main>
+            <aside class = "icone"><p class = "pImage"><img class = "icone1"
+                                        src="images/adresse.jpg"
                                         alt="icone adresse postal">
             </p></aside>
-            
-         </article>   
-        
-        <article>
+
+         </article>
+
+        <article class = "coordonnes2">
             <main><p>Mail : <a href="mailto:tib26@hotmail.fr?Subject=Proposition%20rendez%20vous"
-                     target="_top">tib26@hotmail.fr</a></p></main> 
-            <aside class = "icone"><p class = "pImage"><img class = "icone2" 
-                                                        src="images/mail.jpg" 
+                     target="_top">tib26@hotmail.fr</a></p></main>
+            <aside class = "icone"><p class = "pImage"><img class = "icone2"
+                                                        src="images/mail.jpg"
                                                         alt="icone mail">
             </p></aside>
-         </article>   
-        
-         <article>
-            <main><p> 26 ans </p></main> 
-            <aside class = "icone"><p class = "pImage"><img class = "icone3" 
-                                                        src="images/anniversaire.png" 
+         </article>
+
+         <article class = "coordonnes3">
+            <main><p> 26 ans </p></main>
+            <aside class = "icone"><p class = "pImage"><img class = "icone3"
+                                                        src="images/anniversaire.png"
                                                         alt="icone anniversaire">
             </p></aside>
-         </article> 
-        
-         <article>
-            <main><p>0611757245</p></main> 
-            <aside class = "icone"><p class = "pImage"><img class = "icone4" 
-                                                        src ="images/tel.jpg" 
+         </article>
+
+         <article class = "coordonnes4">
+            <main><p>0611757245</p></main>
+            <aside class = "icone"><p class = "pImage"><img class = "icone4"
+                                                        src ="images/tel.jpg"
                                                         alt ="icone telephone">
             </p></aside>
          </article>
-        
-         <article>
-            <main><p> Permis A, B et véhicule </p></main> 
-            <aside class = "icone"><p class = "pImage"><img class = "icone5" 
-                                                        src ="images/voiture.png" 
+
+         <article class = "coordonnes5">
+            <main><p> Permis A, B et véhicule </p></main>
+            <aside class = "icone"><p class = "pImage"><img class = "icone5"
+                                                        src ="images/voiture.png"
                                                         alt ="icone voiture">
             </p></aside>
          </article>
-        
+
     </div>
 </aside>
-    
-        
+
+
 <section>
-        
-        
+
+
         <h3 class="h3competences">Compétences : </h3>
         <div class = "competences">
             <div class = "licompetences">
-                
-                
+
+
         <li>Html5: </li>
         <li>Css3: </li>
         <li>Php: </li>
@@ -112,66 +102,66 @@
         <li>Linux: </li>
         <li>GIT:</li>
         <li>SCRUM: </li>
-                
+
         </div>
-            
+
         <div class = "liprogress">
-            
-        <progress max="100" value="90" form="form-id">90%</progress>   
+
+        <progress max="100" value="90" form="form-id">90%</progress>
         <progress max="100" value="90" form="form-id">90%</progress>
         <progress max="100" value="50" form="form-id">50%</progress>
-        <progress max="100" value="50" form="form-id">50%</progress> 
+        <progress max="100" value="50" form="form-id">50%</progress>
         <progress max="100" value="70" form="form-id">70%</progress>
-        <progress max="100" value="90" form="form-id">90%</progress> 
         <progress max="100" value="90" form="form-id">90%</progress>
-            
+        <progress max="100" value="90" form="form-id">90%</progress>
+
         </div>
         </div>
-        
+
         <div class = "projet5">
-                 <li>   <p>Toutes mes créations sur : 
+                        <p>Toutes mes créations sur :
                         </p>
                         <p><b><i><a href="http://www.developont.fr/~thibaud/"
                                     target="_blank">http://www.developont.fr/~thibaud/<span> Cliquez pour voir </span>
                                     </a></i></b>
-                            
+
                         </p>
-                 </li>
+
                  </div>
-    
+
 </section>
-    
+
     </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
             <!-----------------------------------------------------------------><div class = "divright"><!----------------------------------------------------------------->
-            
-    
+
+
                                 <main class="blockright">
-       
-       
+
+
                                                                 <!-- H2 -->
-                                                            <h2> Expérience en dévelopement WEB </h2>
+                                                            <h2> Expérience en développement WEB </h2>
     <section>
          <div class = "textCentral1">
-             
+
              <ul class = "ulprojet">
                  <div class = "projet1">
                  <li>   <p>Premiers projet :
-                        </p> 
+                        </p>
                      <p><b><i>Mon curriculum Vitae</i></b>, réalisé seulement 2 semaines apres le debut de la formation developpeur web et mobile. <br>
                             (Cv réalisé tout en HTML et CSS.)
                         </p>
                  </li>
                  </div>
-                 
+
                  <div class = "projet2">
                  <li>   <p>Second projet :
                         </p>
@@ -182,20 +172,20 @@
                         </p>
                  </li>
                  </div>
-                 
+
                  <div class = "projet3">
                  <li>   <p>Troisieme projet :
                         </p>
                         <p><b><i><a href="http://www.developont.fr/~thibaud/travailA5/accueil/accueil.php"
                                     target="_blank">Un site web<span> Cliquez pour voir </span>
-                                    </a> </i></b>, crée pour la formation Develo'Pont. 
+                                    </a> </i></b>, crée pour la formation Develo'Pont.
                                     Site réalisé avec 4 autres apprenants de la formation.  <br>
                                     (Site réalisé en HTML, CSS et PHP.)
-                                
+
                         </p>
                  </li>
                  </div>
-                 
+
                  <div class = "projet4">
                  <li>   <p>Quatrieme projet :
                         </p>
@@ -203,21 +193,21 @@
                                     target="_blank">Un réseau Social<span> Cliquez pour voir </span>
                                     </a></i></b>, réalisé avec 4 autres apprenants de la formation Develo'Pont <br>
                                     (Site réalisé en HTML, CSS, PHP et JAVASCRIPT.)
-                            
+
                         </p>
                  </li>
                  </div>
-                 
+
              </ul>
-                                                                            
+
         </div> <!-- textCentral1 -->
-    </section>                                
-                                    
-                                    
-                                   
+    </section>
+
+
+
                                                                 <!-- H2 -->
                                                             <h2> Formation </h2>
-       
+
     <section>
           <div class="textCentral2">
 
@@ -230,17 +220,17 @@
 
                  <li> 2012-2013: Obtention Cap Mécanique autmobiles - <a href="http://cecof.asso.fr/"
                 target="_blank">C.E.C.O.F<span>Centre Formation Apprentis Centre Consulaire Form.</span> </a>à <a href="https://fr.wikipedia.org/wiki/Amb%C3%A9rieu-en-Bugey"
-                target="_blank">Amberieu-en-bugey<span>Oû se situe Amberieu</span> </a> 
+                target="_blank">Amberieu-en-bugey<span>Oû se situe Amberieu</span> </a>
                  </li> <br>
 
                  <li> 2007-2009: Obtention Cap Vendeur Magasinier - <a href="http://www.cfmda.fr/fr/"
                 target="_blank">CFA Lucien Ravit<span>Centre de Formation Multi-pro Drome-Ardeche</span> </a>à <a href="https://fr.wikipedia.org/wiki/Livron-sur-Dr%C3%B4me"
-                target="_blank">Livron<span>Oû se situe Livron</span> </a> 
+                target="_blank">Livron<span>Oû se situe Livron</span> </a>
                  </li> <br>
 
                  <li> 2006-2007: 1ère année CAP Mecanique - <a href="http://www.cfmda.fr/fr/"
                 target="_blank">CFA Lucien Ravit<span>Centre de Formation Multi-pro Drome-Ardeche</span> </a>à <a href="https://fr.wikipedia.org/wiki/Livron-sur-Dr%C3%B4me"
-                target="_blank">Livron<span>Oû se situe Livron</span> </a> 
+                target="_blank">Livron<span>Oû se situe Livron</span> </a>
                  </li> <br>
 
              </ul>
@@ -248,41 +238,41 @@
 
           </div> <!-- textCentral2 -->
         </section>
-       
-                                    
+
+
                                                             <!-- H2 -->
                                                 <h2> Expériences professionelle </h2>
-       
-       
+
+
     <section>
           <div class="textCentral3">
 
             <ul>
 
-                <li> 2013-2017: Rugbyman semis-professionnel 
+                <li> 2013-2017: Rugbyman semis-professionnel
                 </li> <br>
 
-                <li> 2012-2013: Mécanicien Apprenti - 
+                <li> 2012-2013: Mécanicien Apprenti -
                     <a href="http://www.societe.com/societe/garage-jl-pechoux-352350409.html"
                     target="_blank">Garage Péchoux<span>Quelques informations sur le Garage Péchoux?</span>
                     </a> a
                     <a href="https://fr.wikipedia.org/wiki/Izernore"
-                    target="_blank">Izernore<span>Oû se situe IZERNORE</span> 
-                    </a> 
+                    target="_blank">Izernore<span>Oû se situe IZERNORE</span>
+                    </a>
                 </li> <br>
 
                 <li> Avril a mai 2012: Facteur - La Poste à <a href="https://fr.wikipedia.org/wiki/Oyonnax"
                 target="_blank">Oyonnax<span>Oû se situe OYONNAX</span> </a> et <a href="https://fr.wikipedia.org/wiki/Hauteville-Lompnes"
-                target="_blank">Hauteville<span>Oû se situe HAUTEVILLE</span> </a> 
+                target="_blank">Hauteville<span>Oû se situe HAUTEVILLE</span> </a>
                 </li> <br>
 
                 <li> 2011-2012: Cariste - Agence intérimaire à Oyonnax
                 </li> <br>
 
-                <li> 2010-2011: Vendeur comptoir - 
+                <li> 2010-2011: Vendeur comptoir -
                     <a href="http://www.seigneuriegauthier.com/aspx/index.aspx"
-                    target="_blank">PPGD Comptoir Seigneurie Gauthier<span>Qu'est ce que l'entreprise PPGD</span> 
-                    </a> à 
+                    target="_blank">PPGD Comptoir Seigneurie Gauthier<span>Qu'est ce que l'entreprise PPGD</span>
+                    </a> à
                     <a href="https://fr.wikipedia.org/wiki/Valence_(Dr%C3%B4me)"
                     target="_blank">Valence<span>Oû se situe Valence</span> </a>
                               <ul>
@@ -298,14 +288,14 @@
                 </li>
 
                 <li> 2009-2011: Cariste - Agences interimaires à <a href="https://fr.wikipedia.org/wiki/Loriol-sur-Dr%C3%B4me"
-              target="_blank">Loriol<span>Oû se situe loriol</span> </a> 
+              target="_blank">Loriol<span>Oû se situe loriol</span> </a>
                 </li> <br>
 
                 <li> 2007-2009: Monteur pneumatique apprenti - <a href="http://www.societe.com/societe/pneu-mat-x-489389908.html"
                 target="_blank">Pneu-Mat-X<span>Entreprise Pneu-Mat-X</span>
-                </a> à 
+                </a> à
                 <a href="https://fr.wikipedia.org/wiki/Loriol-sur-Dr%C3%B4me"
-                target="_blank">Loriol<span>Oû se situe loriol</span> 
+                target="_blank">Loriol<span>Oû se situe loriol</span>
                 </a>
 
                                   <ul>
@@ -323,13 +313,13 @@
     </section>
 
 
-                                    
-                                    
+
+
 
                                                             <!-- H2 -->
                                                     <h2> Centre d'interet </h2>
-       
-       
+
+
     <section>
             <div class="textCentral4">
 
@@ -343,11 +333,11 @@
                 </li>
 
                 <li> À l'<a href="http://www.usorugby.com/"
-                            target="_blank">US Oyonnax <span> Union Sportive Oyonnax Rugby </span> </a> de septembre 2011 à aout 2014 
+                            target="_blank">US Oyonnax <span> Union Sportive Oyonnax Rugby </span> </a> de septembre 2011 à aout 2014
                 </li>
 
                 <li> Entrée au club du <a href="http://www.scroyans.fr/"
-                  target="_blank">SC Royannais<span>Sporting Club Royannais</span> </a> en Septembre 2014 
+                  target="_blank">SC Royannais<span>Sporting Club Royannais</span> </a> en Septembre 2014
                 </li>
 
               </ul>
@@ -360,12 +350,12 @@
 
     </section>
     </main>
-  
+
     <footer>
  <a href="#top"> retour en haut de la page</a>
     </footer>
-  
-    </div>  
-    
+
+    </div>
+
 </body>
 </html>
